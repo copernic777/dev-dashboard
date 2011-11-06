6a506e47e47753bfac9266073c2ae934f20df0e5
Damian Lukasik <copernic777@gmail.com>
2011-11-06 22:31:02 +0100

    TimeMachineQuery example

--- src/main/scala/dmnlukasik.sonar.dashboard/Main.scala
+++ src/main/scala/dmnlukasik.sonar.dashboard/Main.scala
@@ -2,7 +2,8 @@ package dmnlukasik.sonar.dashboard
 
 import org.sonar.wsclient.connectors.HttpClient4Connector
 import org.sonar.wsclient.{Host, Sonar}
-import org.sonar.wsclient.services.ResourceQuery
+import org.sonar.wsclient.services.TimeMachineQuery
+import org.scala_tools.time.Imports._
 
 object Main extends App {
   val SONAR_PROJECT_ID = 48569
@@ -10,6 +11,16 @@ object Main extends App {
   val NEMO_SONAR_HOST = "http://nemo.sonarsource.org"
 
   val sonar = new Sonar(new HttpClient4Connector(new Host(NEMO_SONAR_HOST)))
-  val resource = sonar.find(ResourceQuery.createForMetrics(SONAR_PROJECT_ID.toString, "coverage"))
-  println(resource.getMeasures)
+  val timeMachineQuery: TimeMachineQuery = TimeMachineQuery.createForMetrics(SONAR_PROJECT_ID.toString, "coverage", "complexity")
+  timeMachineQuery.setFrom(new DateTime(2011, 11, 6, 0, 0, 0, 0).toDate)
+  val resource = sonar.find(timeMachineQuery)
+
+  resource.getCells.foreach(c => println(c.getValues.deep.toString()))
+  resource.getColumns.foreach(c =>
+    println("metricKey: " + c.getMetricKey +
+      ", index: " + c.getIndex +
+      ", characteristicKey: " + c.getCharacteristicKey +
+      ", modelName: " + c.getModelName +
+      ", date: " + resource.getCells()(c.getIndex).getDate) +
+      ", value: " + resource.getCells()(c.getIndex).getValues.deep.toString())
 }
