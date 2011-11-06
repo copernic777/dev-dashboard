46d0839df0467b5487ecb7a78bc00523b6e86da9
Damian Lukasik <copernic777@gmail.com>
2011-11-06 20:18:52 +0100

    looking for metrics

--- src/main/scala/dmnlukasik.sonar.dashboard/Main.scala
+++ src/main/scala/dmnlukasik.sonar.dashboard/Main.scala
@@ -5,8 +5,11 @@ import org.sonar.wsclient.{Host, Sonar}
 import org.sonar.wsclient.services.ResourceQuery
 
 object Main extends App {
+  val SONAR_PROJECT_ID = 48569
+  val SONAR_PROJECT_RESOURCE_KEY = "org.codehaus.sonar:sonar"
+  val NEMO_SONAR_HOST = "http://nemo.sonarsource.org"
 
-  val sonar = new Sonar(new HttpClient4Connector(new Host("http://nemo.sonarsource.org")))
-  val metrics = sonar.find(ResourceQuery.createForMetrics("48569", "coverage", "lines", "violations"))
-  println(metrics.getMeasure("coverage"))
+  val sonar = new Sonar(new HttpClient4Connector(new Host(NEMO_SONAR_HOST)))
+  val resource = sonar.find(ResourceQuery.createForMetrics(SONAR_PROJECT_ID.toString, "coverage"))
+  println(resource.getMeasures)
 }
